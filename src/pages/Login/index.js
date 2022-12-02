2b9b5c3f089b40e4567366b9a4a2d03744e58bb7
Afiif Imanto <imantoafiif@gmail.com>
2022-12-02 14:42:13 +0700

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,18 +10,16 @@ import style from './Login.module.scss';
 
 function Login() {
     
-    const dispatch = useDispatch()
-    const userSelector = useSelector(userSlice)
     const navigate = useNavigate()
-    const [user, setUser] = useState('')
-    const [password, setPassword] = useState('')
+    const user = useRef()
+    const password = useRef()
+    const [isLoginError, setIsLoginError] = useState(false)
     const [isLoading, setLoading] = useState(false)
     const [remember, setRemember] = useState(false)
     const [selectedMethod, setSelectedMethod] = useState('LDAP')
     const [passwordVisible, setPasswordVisible] = useState(false)
     const account = useContext(AccountProvider)
     const secret = `I!OJ1n4!Nl$cmtv5aB^KK3xV5jNlB72RYSg7OrmoyCBmFpfpmFeZ51LsQ%.VNrNE>uw<P>rnJn6^1!$k>3>TPg'q(B~TBPvRH8/i(s$ucgz+/+e!|`
-    var careerPositionAspiration = null;
     const methods = [
         { value: 'LDAP', label: 'LDAP' }, 
         { value: 'SSO', label: 'SSO' }
@@ -30,21 +28,18 @@ function Login() {
     useEffect(() => {
         let uname = localStorage.getItem('auth.user')
         let pass = localStorage.getItem('auth.pass')
-        if(uname && pass) {
-            setUser(CryptoJS.AES.decrypt(uname, secret).toString(CryptoJS.enc.Utf8))
-            setPassword(CryptoJS.AES.decrypt(pass, secret).toString(CryptoJS.enc.Utf8))
-            setRemember(true)
-        }
+        // if(uname && pass) {
+        //     setUser(CryptoJS.AES.decrypt(uname, secret).toString(CryptoJS.enc.Utf8))
+        //     setPassword(CryptoJS.AES.decrypt(pass, secret).toString(CryptoJS.enc.Utf8))
+        //     setRemember(true)
+        // }
     }, [])
 
-    useEffect(() => {
-        console.log(selectedMethod)
-    }, [selectedMethod])
-
-    const login = (e) => {
-        console.log('remember', remember)
+    const login = async e => {
+        console.log('remember', user)
         e.preventDefault()
         setLoading(true)
+        setIsLoginError(false)
 
         let uname = localStorage.getItem('auth.user')
         let pass = localStorage.getItem('auth.pass')
@@ -59,10 +54,42 @@ function Login() {
             localStorage.setItem('auth.pass', CryptoJS.AES.encrypt(password, secret))
         }
 
+        let sso = null
+        if(selectedMethod === 'SSO') {
+            console.log(selectedMethod)
+            sso = await axios.get(`https://aktivis-api.pnm.co.id/v1/sso/webservice/login`, {
+                params: {
+                    user: user.current.value,
+                    pass: password.current.value,
+                    app_code: 'ITMS',
+                },
+                auth: {
+                    username: 'event',
+                    password: 'event',
+                }
+            })
+            .then(r => {
+                if(r.data.login[0].response === 'TRUE') {
+                    return r.data.login[0]
+                }
+                setIsLoginError(true)
+                return null
+            })
+            .catch(e => {
+                setIsLoginError(true)
+                console.log(e)
+            })
+
+            if(!sso) {
+                setLoading(false)
+                return
+            }
+        }
+
         axios.post('/ldap/api/auth/login', {
             application_id: process.env.REACT_APP_ID,
-            password,
-            username: user,
+            password : sso ? 'secret' : password.current.value,
+            username: sso ? sso.data[0].nik : user.current.value,
         })
         .then(r => {
             // setLoading(false)
@@ -77,7 +104,6 @@ function Login() {
                     // console.log('cuk', re)
                     // let session = JSON.stringify(re.data.data)
                     // localStorage.setItem('auth.session', session)
-                    dispatch(setSession(re.data.data))
                     account.set(re.data.data)
                     console.log('mboke', account)
                     navigate('/home')
@@ -88,6 +114,7 @@ function Login() {
         })
         .catch(e => {
             setLoading(false)
+            setIsLoginError(true)
             console.log(e)
         })
     }
@@ -134,6 +161,7 @@ function Login() {
                         <div className={style.login_input__field}>
                             <i className="fa fa-user"></i>
                             <input 
+                                ref={user}
                                 required
                                 type="text"
                                 autoComplete="on"/>
@@ -146,6 +174,7 @@ function Login() {
                         <div className={style.login_input__field}>
                             <i className="fa fa-key"></i>
                             <input 
+                                ref={password}
                                 required
                                 type={passwordVisible ? 'text' : 'password'}
                                 autoComplete="on"/>
@@ -157,15 +186,24 @@ function Login() {
                         </div>
                     </div>
                 </div>
+                {
+                    isLoginError &&
+                    (
+                        <div className={`${style.login_error} has-text-centered has-text-weight-bold is-uppercase has-text-danger`}>
+                            <span>You have entered invalid credentials</span>
+                        </div>
+                    ) 
+                }
                 <div style={{display: "flex", justifyContent: "center", padding: "1rem 0", marginBottom: "1rem"}}>
                     <button 
+                        disabled={isLoading}
                         type="submit"
                         className={style.login_page_button}>
                         {
                             isLoading ? 
                             (
-                                <span className="login-page-button__spinner">
-                                    <i clasclassNames="fa fa-spinner"></i>
+                                <span className={style.login_page_button__spinner}>
+                                    <i className="fa fa-spinner"></i>
                                 </span>
                             ) :
                             (<span>LOGIN</span>)
