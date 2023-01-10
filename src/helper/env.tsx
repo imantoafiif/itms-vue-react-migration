19e6007e1c4486d56df3cc7bde277178d26fb40f
Afiif Imanto <imantoafiif@gmail.com>
2023-01-11 01:11:01 +0700

    .env

--- src/helper/env.tsx
+++ src/helper/env.tsx
@@ -1,4 +1,11 @@
-export const BASE_URL:string = process.env.REACT_APP_API_BASE_URL as string
-export const APP_ID:string = process.env.REACT_APP_ID as string
-export const BUSCD:string = process.env.REACT_APP_BUSCD as string
-export const SECRET:string = process.env.REACT_APP_SECRET as string
+const BASE_URL = process.env.REACT_APP_API_BASE_URL as string
+const APP_ID = process.env.REACT_APP_ID as string
+const BUSCD = process.env.REACT_APP_BUSCD as string
+const SECRET = process.env.REACT_APP_SECRET as string
+
+export { 
+    BASE_URL,
+    APP_ID,
+    BUSCD,
+    SECRET
+ }
